c3e4d43705a17c7bf80f7d4709ee220ebaf5260c
Nitin Mesta <nitinmesta@Raghus-MacBook-Pro.local>
2020-11-09 14:51:43 +0530

    Init

---
+++ app/src/main/java/com/example/rakuten/ui/DetailsPage.kt
@@ -0,0 +1,12 @@
+package com.example.rakuten.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.rakuten.R
+
+class DetailsPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details_page)
+    }
+}
